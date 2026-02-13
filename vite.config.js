bf8f547e5bff4c6db57600bd984536cefa53f5f5
albrechtlara <daniel.dante@me.com>
2026-02-13 13:58:05 -0300

    Ajuste para deploy na Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Removida a configuração de 'base' para que funcione corretamente na Vercel
 })
